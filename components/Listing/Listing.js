ab25ec4285fdb28102c1013bcf80890b4efbca04
Saloni-FED <salonipandey014@gmail.com>
2024-07-10 14:42:31 +0530

    Search added

--- components/Listing/Listing.js
+++ components/Listing/Listing.js
@@ -1,78 +1,82 @@
-import React from 'react';
-import ListingCard from './ListingCard';
-
-const data = [
-  {
-    image: '/images/history/history1.png',
-    title: 'A V Thomas & Co. Limited Unlisted Shares',
-    price: 12500,
-    change: 0,
-    changePercent: 0,
-    days: 15,
-  },
-  {
-    image: '/images/history/history1.png',
-    title: 'ACS Technologies Limited Unlisted Shares',
-    price: 46,
-    change: 0,
-    changePercent: 0,
-    days: 15,
-  },
-  {
-    image: '/images/history/history1.png',
-    title: 'ACS Technologies Limited Unlisted Shares',
-    price: 46,
-    change: 0,
-    changePercent: 0,
-    days: 15,
-  },
-  {
-    image: '/images/history/history1.png',
-    title: 'ACS Technologies Limited Unlisted Shares',
-    price: 46,
-    change: 0,
-    changePercent: 0,
-    days: 15,
-  },
-  {
-    image: '/images/history/history1.png',
-    title: 'ACS Technologies Limited Unlisted Shares',
-    price: 46,
-    change: 0,
-    changePercent: 0,
-    days: 15,
-  },
-  {
-    image: '/images/history/history1.png',
-    title: 'ACS Technologies Limited Unlisted Shares',
-    price: 46,
-    change: 0,
-    changePercent: 0,
-    days: 15,
-  },
-];
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
 
 const Listing = () => {
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const fetchPartners = async () => {
+    try {
+      const contactsRef = collection(db, "unlistedShares");
+      const snapshot = await getDocs(contactsRef);
+      const contactsList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log(contactsList);
+      setProducts(contactsList);
+    } catch (error) {
+      console.error("Error fetching partners:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPartners();
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="container">
       <div>
-        <h2 style={{textAlign:"center", marginTop:"1rem", marginBottom:"2rem"}}>Unlisted Shares</h2>
+        <h2
+          style={{
+            textAlign: "center",
+            marginTop: "1rem",
+            marginBottom: "2rem",
+          }}
+        >
+          Unlisted Shares
+        </h2>
       </div>
       <div className="search-bar">
-        <input type="text" placeholder="Search..." className="search-input" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search-input"
+          value={searchQuery}
+          onChange={handleSearch}
+        />
       </div>
       <div className="grid">
-        {data.map((item, index) => (
-          <ListingCard
-            key={index}
-            image={item.image}
-            title={item.title}
-            price={item.price}
-            change={item.change}
-            changePercent={item.changePercent}
-            days={item.days}
-          />
-        ))}
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((item, index) => (
+            <ListingCard
+              key={index}
+              image={item.imageUrl}
+              title={item.name}
+              price={item.price}
+            />
+          ))
+        ) : (
+          <p
+            style={{
+              textAlign: "center",
+              marginTop: "2rem",
+              gridColumn: "span 2",
+            }}
+          >
+            No Shares Found!
+          </p>
+        )}
       </div>
     </div>
   );
--- components/Listing/ListingCard.js
+++ components/Listing/ListingCard.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
+const truncateTitle = (title, wordLimit) => {
+  const words = title.split(' ');
+  if (words.length > wordLimit) {
+    return words.slice(0, wordLimit).join(' ') + '...';
+  }
+  return title;
+};
+
 const ListingCard = ({ image, title, price, change, changePercent, days }) => {
+  const truncatedTitle = truncateTitle(title, 2);
+
   return (
     <div className="card">
       <div className='image-fit'>
-        <img src={image} width={100} height={100}/>
+        <img src={image} width={50} height={50} style={{width:"200px", height:"100"}}/>
       </div>
-      <div>
-      <p className="card-title">{title}</p>
-        <p className="card-price">₹{price}</p>
+      <div style={{width:"10rem"}}>
+        <p className="card-title">{truncatedTitle}</p>
+        <p className="">₹{price}</p>
         {/* <p className={`card-change ${change >= 0 ? 'positive' : 'negative'}`}>
           {change >= 0 ? '+' : '-'}{Math.abs(change)} ({changePercent}%)
         </p>
         <p className="card-days">{days}D</p> */}
       </div>
-      
     </div>
   );
 };
